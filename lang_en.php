c80d9c14e7d6254c0287d5b0f486566a51d1fac0
Marvin Ludwig <MarvinLudwig@users.noreply.github.com>
2015-06-21 14:39:42 +0200

    Create lang_en.php

---
+++ lang_en.php
@@ -0,0 +1,16 @@
+<?php
+
+$msg['amount_invalid'] = "Amount is not a valid number";
+$msg['amount_decimals'] = "Amount must not have more than 2 decimals."
+$msg['encoding_1'] = "Character encoding";
+$msg['encoding_2'] = "is not supported."
+$msg['name_length'] = "Name has more than 70 characters.";
+$msg['bic_length'] = "BIC has more than 11 characters.";
+$msg['iban_length'] = "IBAN has more than 34 characters.";
+$msg['amount_length'] = "Amount has more than 12 characters.";
+$msg['text_length'] = "Reference text has more than 140 characters.";
+$msg['payload_bytes_1'] = "Total payload is more than 331 bytes";
+$msg['payload_bytes_2'] = "The reference text has";
+$msg['bytes'] = "bytes";
+
+?>
