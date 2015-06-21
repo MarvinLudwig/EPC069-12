7320b0da8b0bc7d98344e50fb020859b80ce712f
Marvin Ludwig <MarvinLudwig@users.noreply.github.com>
2015-06-21 14:39:21 +0200

    Create lang_de.php

---
+++ lang_de.php
@@ -0,0 +1,16 @@
+<?php
+
+$msg['amount_invalid'] = "Der Betrag ist keine gültige Zahl";
+$msg['amount_decimals'] = "Der Betrag darf nicht mehr als 2 Nachkommastellen enthalten."
+$msg['encoding_1'] = "Zeichenkodierung";
+$msg['encoding_2'] = "wird nicht unterstützt."
+$msg['name_length'] = "Der Name darf nicht aus mehr als 70 Zeichen bestehen.";
+$msg['bic_length'] = "Der BIC darf nicht aus mehr als 11 Zeichen bestehen.";
+$msg['iban_length'] = "Die IBAN darf nicht aus mehr als 34 Zeichen bestehen.";
+$msg['amount_length'] = "Der Betrag darf nicht aus mehr als 12 Zeichen bestehen.";
+$msg['text_length'] = "Der Verwendungszweck darf nicht aus mehr als 140 Zeichen bestehen.";
+$msg['payload_bytes_1'] = "Die Daten beinhalten mehr als 331 Byte";
+$msg['payload_bytes_2'] = "Der Verwendungszweck besteht aus";
+$msg['bytes'] = "Byte";
+
+?>
